f1eae10c1b1b6e12695757c6fde92b5e7a3954ea
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-05-25 00:16:28 +0800

    remove routing logic to facilitate rendering

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,14 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from '@/hooks/AuthContext';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
+  // const router = useRouter();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -17,13 +19,27 @@ export default function RootLayout() {
     return null;
   }
 
+  // mock for now
+  // const user = null;
+  // const loading = false;
+
+  // useEffect(() => {
+  //   if (!loading && !user) {
+  //     router.replace('(auth)/login');
+  //   }
+  // }, [user, loading])
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        {/*<Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>*/}
+        <Slot />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
