17232cd913238ba7247058ad7dbec9245d000719
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-06-19 22:29:41 +0800

    add typing based on database table structures

--- mobile/types/types.ts
+++ mobile/types/types.ts
@@ -1,6 +1,16 @@
-export type Message = {
+export type User = {
     id: number,
-    room_id: number,
+    name: string,
+    email: string,
+    password: string,
+    last_seen: string,
+    points: number,
+    interests: string[]
+}
+
+export type Message = {
+    id?: number,
+    room_id: string,
     sender_id: number,
     receiver_id: number,
     content: string,
