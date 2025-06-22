792bcf510575180d1d1eb97775f5c08a7ee5ea7b
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-06-22 20:09:20 +0800

    add dependencies and config

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,3 +1,4 @@
+const ip = require('ip');
 import 'dotenv/config';
 import type { ExpoConfig } from 'expo/config';
 
@@ -6,5 +7,6 @@ export default (): ExpoConfig => ({
     slug: 'nuseek',
     extra: {
         EXPRESS_HOST_URL: process.env.EXPRESS_HOST_URL,
+        SOCKET_HOST: `ws://${ip.address()}:3000`,
     },
 });
