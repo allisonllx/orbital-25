d36f50e50507f9d5007c25dbdd0dc4a063eccffb
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-06-23 21:38:26 +0800

    shift openai instance into the function for ease of testing

--- backend/utils.js
+++ backend/utils.js
@@ -1,8 +1,8 @@
 const OpenAI = require('openai');
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
 async function generateEmbeddings(text) {
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+    
     const response = await openai.embeddings.create({
         model: "text-embedding-3-small",
         input: text,
