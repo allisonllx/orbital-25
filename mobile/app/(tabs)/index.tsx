48e5df2a195695524022e8034ad52344e23b4f00
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-06-26 01:47:19 +0800

    render relevant components for task display

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,7 +1,38 @@
-import { TopBanner } from '@/components/TopBanner.js';
+import { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { Task } from '@/types/types';
+import { TaskList } from '@/components/TaskList';
+import { TopBanner } from '@/components/TopBanner';
 
 export default function HomeScreen() {
-    return (
-        <TopBanner />
-    )
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const host = Constants.expoConfig?.extra?.EXPRESS_HOST_URL ?? 'http://localhost:3000';
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const res = await fetch(`${host}/tasks`);
+        const data = await res.json();
+        setTasks(data);
+      } catch (err) {
+        console.error('Error fetching tasks:', err);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TopBanner />
+      <TaskList tasks={tasks} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
