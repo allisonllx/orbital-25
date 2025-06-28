8e6e32063cba189b4ac265ff4f0add641437410e
Allison Law Li Xuan <allisonlawlixuan@Allisons-Laptop.local>
2025-06-28 23:58:43 +0800

    modify api and socket host

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,12 +1,19 @@
 const ip = require('ip');
-import 'dotenv/config';
-import type { ExpoConfig } from 'expo/config';
+import type { ExpoConfig, ConfigContext } from 'expo/config';
 
-export default (): ExpoConfig => ({
-    name: 'NUSeek',
-    slug: 'nuseek',
-    extra: {
-        EXPRESS_HOST_URL: process.env.EXPRESS_HOST_URL,
-        SOCKET_HOST: `ws://${ip.address()}:3000`,
-    },
-});
+export default ({ config } : ConfigContext): ExpoConfig => {
+    const apiHost = process.env.EXPRESS_HOST_URL ?? `http://${ip.address()}:3000`;
+
+    const socketHost =
+        process.env.SOCKET_HOST ??
+        apiHost.replace(/^http(s?):/, 'ws$1:'); // ws://ip:3000 or wss://prod
+
+    return {
+        name: 'NUSeek',
+        slug: 'nuseek',
+        extra: {
+            EXPRESS_HOST_URL: apiHost,
+            SOCKET_HOST: socketHost,
+        },
+    };
+};
